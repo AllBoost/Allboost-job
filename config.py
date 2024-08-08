ac0d79409424a027ea186c196a3a76a991c2e8ff
AllBoost <knnvsan@yandex.ru>
2024-08-08 22:57:06 +0300

    Update and rename config.php to config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+db_host = '147.45.168.161'  # Публичный IP
+db_name = 'Daring Bittern'  # Название базы данных
+db_user = 'gen_user'        # Логин пользователя
+db_pass = '?(Ls\\g)60\\ayNO'  # Пароль пользователя
